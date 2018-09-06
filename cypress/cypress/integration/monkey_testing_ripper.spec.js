da025e72903c0f3d2ed08be3889c96f6a9a4ab17
makito <makitodev@outlook.com>
2018-09-06 01:03:01 -0500

    create a skeleton of solution example. (methods not implemented yet)

--- cypress/cypress/integration/monkey_testing_ripper.spec.js
+++ cypress/cypress/integration/monkey_testing_ripper.spec.js
@@ -2,17 +2,18 @@ describe('Los estudiantes under monkeys', function() {
     it('visits los estudiantes and survives monkeys', function() {
         cy.visit('https://losestudiantes.co');
         cy.contains('Cerrar').click();
-        cy.wait(1000);
+        cy.wait(500);
         randomClick(10);
     })
 })
+//Random function
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+};
+//Random clic
 function randomClick(monkeysLeft) {
-    function getRandomInt(min, max) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min)) + min;
-    };
-
     var monkeysLeft = monkeysLeft;
     if(monkeysLeft > 0) {
         cy.get('a').then($links => {
@@ -24,4 +25,25 @@ function randomClick(monkeysLeft) {
             setTimeout(randomClick, 1000, monkeysLeft);
         });
     }   
+}
+
+//Random events
+function randomEvent(eventNum){
+    switch (eventNum) {
+        case 1://click on link
+            randomClick(monkeysLeft);
+            break;
+        case 2://Text field
+            randomTextField(monkeysLeft)
+            break;
+        case 3://combo select
+            randomComboSelect(monkeysLeft)
+            break;
+        case 4://click on button
+            randomClickButton(monkeysLeft)
+            break;
+        default:
+        console.log(`Option not available`);
+            break;
+    }
 }
