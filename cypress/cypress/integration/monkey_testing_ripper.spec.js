745f2eb6d7f2728e70e7fb12b2fc85b0a98797c5
makito <makitodev@outlook.com>
2018-09-07 23:30:40 -0500

    MonkeyTest with events fine :)

--- cypress/cypress/integration/monkey_testing_ripper.spec.js
+++ cypress/cypress/integration/monkey_testing_ripper.spec.js
@@ -3,18 +3,9 @@ describe('Los estudiantes under monkeys', function() {
         cy.visit('https://losestudiantes.co');
         cy.contains('Cerrar').click();
         cy.wait(500);
-        randomClick(10);
-    })
-})
-
-describe('HomeWork monkeys', function() {
-    it('visits los estudiantes and execute random monkeys', function() {
-        cy.visit('https://losestudiantes.co');
-        cy.contains('Cerrar').click();
-        cy.wait(500);
-        ///Select random event
-        let event = getRandomInt(1,4);
-        randomEvent(event, 10)
+        //randomClick(10);
+        var eventnumber = getRandomInt(1, 5)
+        randomEvent(eventnumber, 5)
     })
 })
 
@@ -34,13 +25,15 @@ function randomClick(monkeysLeft) {
                 cy.wrap(randomLink).click({force: true});
                 monkeysLeft = monkeysLeft - 1;
             }
-            setTimeout(randomClick, 1000, monkeysLeft);
+            cy.wait(1000);
+            randomClick(monkeysLeft);
+            //setTimeout(randomClick, 1000, monkeysLeft);
         });
     }   
 }
 //Random text
 function randomSetTextInput(monkeysLeft){
-    let monkeysLeft = monkeysLeft;
+    //let monkeysLeft = monkeysLeft;
     if(monkeysLeft > 0) {
         cy.get('input[type="text"]').then($inputs => {
             console.log(`Number of inputs: ${$inputs.length}`)
@@ -53,10 +46,10 @@ function randomSetTextInput(monkeysLeft){
 }
 //randomComboSelect
 function randomComboSelect(monkeysLeft){
-    let monkeysLeft = monkeysLeft;
+    //let monkeysLeft = monkeysLeft;
     if(monkeysLeft > 0){
-        cy.get('select').then($combos => {
-            let randomInput = $combos.get(getRandomInt(0, $combos.length));
+        cy.get('select').then($selects => {
+            var randomInput = $selects.get(getRandomInt(0, $selects.length));
             var option = randomInput.options[getRandomInt(0, randomInput.options.length)];
             cy.wrap(randomInput).select(option.value, { force: true });
             monkeysLeft = monkeysLeft - 1;
@@ -66,30 +59,67 @@ function randomComboSelect(monkeysLeft){
 }
 //Random ClickButton
 function randomClickButton(monkeysLeft){
-    let monkeysLeft = monkeysLeft;
-    cy.get('button').then($inputs => {
-        var randomInput = $inputs.get(getRandomInt(0, $inputs.length));
-        if (!Cypress.dom.isHidden(randomInput)) {
-            cy.wrap(randomInput).click({ force: true });
-            monkeysLeft = monkeysLeft - 1;
-        }
-        setTimeout(randomClickButton, 1000, monkeysLeft);
-    });
+    //let monkeysLeft = monkeysLeft;
+    if(monkeysLeft > 0){
+        cy.get('button').then($inputs => {
+            var randomInput = $inputs.get(getRandomInt(0, $inputs.length));
+            if (!Cypress.dom.isHidden(randomInput)) {
+                cy.wrap(randomInput).click({ force: true });
+                monkeysLeft = monkeysLeft - 1;
+            }
+            setTimeout(randomClickButton, 1000, monkeysLeft);
+        });
+    }
 }
 //Random events
 function randomEvent(eventNum, monkeysLeft){
+    var monkeysLeft = monkeysLeft;
     switch (eventNum) {
         case 1://click on link
-            randomClick(monkeysLeft);
+            if(monkeysLeft > 0) {
+                cy.get('a').then($links => {
+                    var randomLink = $links.get(getRandomInt(0, $links.length));
+                    if(!Cypress.dom.isHidden(randomLink)) {
+                        cy.wrap(randomLink).click({force: true});
+                        monkeysLeft = monkeysLeft - 1;
+                    }
+                    setTimeout(randomEvent(eventNum, monkeysLeft), 1000);
+                });
+            }
             break;
         case 2://Text field
-            randomSetTextInput(monkeysLeft)
+            if(monkeysLeft > 0) {
+                cy.get('input[type="text"]').then($inputs => {
+                    console.log(`Number of inputs: ${$inputs.length}`)
+                    let randomInputText = $inputs.get(getRandomInt(0, $inputs.length));
+                    cy.wrap(randomInputText).type("-ABC123DEF456", { force: true });
+                    monkeysLeft = monkeysLeft - 1;
+                    setTimeout(randomEvent(eventNum, monkeysLeft), 1000);
+                });
+            }
             break;
         case 3://combo select
-            randomComboSelect(monkeysLeft)
+            if(monkeysLeft > 0){
+                cy.get('select').then($selects => {
+                    var randomInput = $selects.get(getRandomInt(0, $selects.length));
+                    var option = randomInput.options[getRandomInt(0, randomInput.options.length)];
+                    cy.wrap(randomInput).select(option.value, { force: true });
+                    monkeysLeft = monkeysLeft - 1;
+                    setTimeout(randomEvent(eventNum, monkeysLeft), 1000);
+                });
+            }
             break;
         case 4://click on button
-            randomClickButton(monkeysLeft)
+            if(monkeysLeft > 0){
+                cy.get('button').then($inputs => {
+                    var randomInput = $inputs.get(getRandomInt(0, $inputs.length));
+                    if (!Cypress.dom.isHidden(randomInput)) {
+                        cy.wrap(randomInput).click({ force: true });
+                        monkeysLeft = monkeysLeft - 1;
+                    }
+                    setTimeout(randomEvent(eventNum, monkeysLeft), 1000);
+                });
+            }
             break;
         default:
         console.log(`Option not available`);
